921b6e5823eaf76c3355c48143a229d20da609c8
FranLisboa <110274821+FranLisboa@users.noreply.github.com>
2023-02-07 19:06:16 -0300

    funções adicionadas e correção de bugs
    Adicionada a função de colocar números decimais, limitação dos caracteres no display dos numeros para 12 (nunca ficando pra fora do display) e correção de bug com o botão de igual

--- Calculadora/script.js
+++ Calculadora/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+let stringNumber1 = '';
 let number1 = 0;
 let operation = '';
 let number2 = 0;
@@ -19,40 +20,82 @@ const equalBtn = document.querySelector('.equal');
 
 const resetBtn = document.querySelector('.reset');
 
+const deciamlBtn = document.querySelector('.decimal');
+
+let content;
+
+let lastAction = true;
+
 for (let i = 0; i < numberBtns.length; i++) {
   numberBtns[i].addEventListener('click', function () {
-    let content = document.querySelector('.display').textContent;
-    number1 = Number(content + numberBtns[i].textContent);
-    document.querySelector('.display').textContent = number1;
+    if (stringNumber1.length < 12) {
+      content = document.querySelector('.display').textContent;
+      stringNumber1 = content + numberBtns[i].textContent;
+      number1 = Number(stringNumber1);
+
+      document.querySelector('.display').textContent = number1;
+    }
   });
-  //12 max cap
 }
 
 for (let i = 0; i < operationsBtns.length; i++) {
   operationsBtns[i].addEventListener('click', function () {
     operation = operationsBtns[i].textContent;
-    console.log(operation);
+
     number2 = number1;
+    stringNumber1 = '';
+    number1 = 0;
     document.querySelector('.display').textContent = '';
+    lastAction = true;
   });
 }
 
 equalBtn.addEventListener('click', function () {
-  if (operation === '+') {
-    number1 = number2 + number1;
-  } else if (operation === '-') {
-    number1 = number2 - number1;
-  } else if (operation === 'x') {
-    number1 = number2 * number1;
-  } else if (operation === '/') {
-    number1 = number2 / number1;
+  if (lastAction) {
+    if (operation === '+') {
+      number1 = number2 + number1;
+    } else if (operation === '-') {
+      number1 = number2 - number1;
+    } else if (operation === 'x') {
+      number1 = number2 * number1;
+    } else if (operation === '/') {
+      number1 = number2 / number1;
+    }
+
+    stringNumber1 = String(number1);
+    if (stringNumber1.length >= 12) {
+      if (Number(stringNumber1[12]) > 5) {
+        let newNum = Number(stringNumber1[11]) + 1;
+        stringNumber1 = stringNumber1.substring(0, 11) + newNum;
+      }
+      stringNumber1 = stringNumber1.substring(0, 13);
+      number1 = Number(stringNumber1);
+    }
+
+    document.querySelector('.display').textContent = number1;
+    lastAction = false;
   }
-  document.querySelector('.display').textContent = number1;
 });
 
 resetBtn.addEventListener('click', function () {
+  stringNumber1 = '';
   number1 = 0;
   operation = '';
   number2 = 0;
   document.querySelector('.display').textContent = '';
+  lastAction = true;
+});
+
+deciamlBtn.addEventListener('click', function () {
+  let confirm = true;
+  for (let i = 0; i < stringNumber1.length; i++) {
+    if (stringNumber1[i] === '.') {
+      confirm = false;
+    }
+  }
+  if (confirm == true) {
+    stringNumber1 += '.';
+
+    document.querySelector('.display').textContent = stringNumber1;
+  }
 });
